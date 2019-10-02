106e0160730223a6db335a56d751defa23e7b9a5
Jeremy Kairis <jeremy.kairis@mi8.be>
2019-10-02 22:34:39 +0200

    Remove PropTypes

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
@@ -56,14 +55,6 @@ class BlogRoll extends React.Component {
       </div>
     )
   }
-}
-
-BlogRoll.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
 }
 
 export default () => (
